20e045ffd93a5ca3e0852b4a782885a5b527dffd
dastangoo <hamed.dastangoo@gmail.com>
2019-04-09 15:47:02 +0430

    Add some examples

---
+++ myNumber.js
@@ -0,0 +1,14 @@
+// var a = "123";
+// var newNum = new Number()
+// console.log("NewNum value", newNum);
+// newNum  instanceof Number
+
+// var b = Number("123");
+// console.log(b);
+// b instanceof Number
+
+var myVal = ["0"];
+// console.log("the value of", myVal.valueOf());
+// console.log("to string", myVal.toString(), typeof myVal.toString())
+
+console.log(Number(myVal))
