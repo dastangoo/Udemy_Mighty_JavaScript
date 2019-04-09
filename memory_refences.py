8f1a1ee50069ee49ade23e4157096aad03189efd
dastangoo <hamed.dastangoo@gmail.com>
2019-04-09 13:25:03 +0430

    add some examples

---
+++ memory_refences.py
@@ -0,0 +1,24 @@
+# my_number = 2019
+# print("Hex address of my_number", hex(id(my_number)))
+# print("{0}".format("-" * 50))
+#
+# my_string = "Hello Python from JavaScript"
+# print("Memory address of my_string", hex(id(my_string)))
+# print("{0}".format("-" * 50))
+#
+# my_variable = "this is my variable"
+# print("memory address of my_variable", hex(id(my_variable)))
+# print("{0}".format("-" * 50))
+#
+#
+# my_new_number = my_number
+# print("memory address of my_new_number", hex(id(my_new_number)))
+# print("{0}".format("-" * 50))
+
+my_new_number = 2020
+print("memory address of my_new_number", hex(id(my_new_number)))
+print("{0}".format("-" * 50))
+
+my_new_number += 1
+print("memory address of my_new_number", hex(id(my_new_number)))
+print("{0}".format("-" * 50))
