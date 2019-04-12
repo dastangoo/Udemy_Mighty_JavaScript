d1b62fb729f4c93b0176775c44b09d830f953ab4
dastangoo <hamed.dastangoo@gmail.com>
2019-04-12 12:30:56 +0430

    Add some examples

---
+++ shortCircuitEvaluation.js
@@ -0,0 +1,11 @@
+var name = "Jasmine";
+
+// if (name) {
+//   if (name.length > 1) {
+//     console.log(`Hello ${name}.`);
+//   }
+// }
+
+if (name && name.length > 1) {
+  console.log(`Hello ${name}.`);
+}
